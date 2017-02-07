81968b356c4205544a86aefcef204510a44a7ad8
opas350 <opas350@gmail.com>
2017-02-07 00:43:34 -0600

    Update files

--- PrototypePattern/Shape.java
+++ PrototypePattern/Shape.java
@@ -6,4 +6,26 @@ public abstract class Shape implements Cloneable {
 	abstract void draw();
 
 	public String getType() {
-		return type
+		return type;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Object clone() {
+		Object clone = null;
+
+		try {
+			clone = super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+
+		return clone;
+	}
+}
